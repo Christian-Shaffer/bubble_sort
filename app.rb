e2b6c67851581676b537041361f8166ffa664e54
Christian Shaffer <calexandershaffer@gmail.com>
2023-10-26 20:59:45 -0400

    Complete code, optimized to break early when necesssary

--- app.rb
+++ app.rb
@@ -2,15 +2,18 @@
 def bubble_sort(array)
   temp_value = 0
   (array.length - 1).times do
+    swapped = false
     array.each_index do |index|
       if index < array.length - 1 && array[index] > array[index + 1]
         temp_value = array[index]
         array[index] = array[index + 1]
         array[index + 1] = temp_value
+        swapped = true
       end
     end
+    break unless swapped
   end
   array
 end
 
-p bubble_sort([9,6,5,3,2])
+p bubble_sort([1,2,3])
