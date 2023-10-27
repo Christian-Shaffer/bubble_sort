fa7c6fb2f268a0a7e97090297587f3537dbb1675
Christian Shaffer <calexandershaffer@gmail.com>
2023-10-26 21:03:23 -0400

    Add comments for readability

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 
 def bubble_sort(array)
-  temp_value = 0
+  temp_value = 0 # Placeholder to help swapping one value to another
   (array.length - 1).times do
     swapped = false
     array.each_index do |index|
+      # Prevent checking of nil indexes
       if index < array.length - 1 && array[index] > array[index + 1]
         temp_value = array[index]
         array[index] = array[index + 1]
@@ -11,9 +12,10 @@ def bubble_sort(array)
         swapped = true
       end
     end
-    break unless swapped
+    break unless swapped # Will stop early to save memory if no swap occurred
   end
   array
 end
 
-p bubble_sort([1,2,3])
+p bubble_sort([99,13,-1,6,3,2,4,33,-5,6])
+# Output: [-5, -1, 2, 3, 4, 6, 6, 13, 33, 99]
