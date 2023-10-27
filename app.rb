029a7e7ee04ee9ed449786ab4803aeabe9f6f066
Christian Shaffer <calexandershaffer@gmail.com>
2023-10-26 20:28:45 -0400

    Remove irrelevant variable

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ def bubble_sort(array)
         temp_value = array[index]
         array[index] = array[index + 1]
         array[index + 1] = temp_value
-        swap_occurred = true
       end
     end
   end
