563285f6d1d53b4dd58224566e9fa20b60dca2fd
Christian Shaffer <calexandershaffer@gmail.com>
2023-10-27 01:05:38 -0400

    Update swapped variable name for clarification

--- app.rb
+++ app.rb
@@ -2,16 +2,16 @@
 def bubble_sort(array)
   temp_value = 0 # Placeholder to help swapping one value to another
   (array.length - 1).times do
-    swapped = false
+    swap_occurred = false
     array.each_index do |index|
       if index < array.length - 1 && array[index] > array[index + 1]
         temp_value = array[index]
         array[index] = array[index + 1]
         array[index + 1] = temp_value
-        swapped = true
+        swap_occurred = true
       end
     end
-    break unless swapped # Will stop early to save memory if no swap occurred
+    break unless swap_occurred # Will stop early to save memory if no swap occurred
   end
   array
 end
